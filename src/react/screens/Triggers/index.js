96dc2ad3d5ab68c48c08293a0a101264ef0b3004
Dheeraj Yalamanchili <dhee9000@gmail.com>
2019-09-07 09:25:06 -0400

    adds FAB and changes scroll view to flat list

--- src/react/screens/Triggers/index.js
+++ src/react/screens/Triggers/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { View, ScrollView } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
+import { FloatingAction } from "react-native-floating-action";
 
 import * as ActionTypes from '../../../redux/ActionTypes';
 import { connect } from 'react-redux';
 
 import { Heading, Text } from '../../components/StyledComponents';
-import { MaterialCommunityIcons as Icon } from '@expo/vector-icons'
+import { Feather as Icon } from '@expo/vector-icons'
 
 
 class EditProfile extends React.Component{
@@ -16,13 +17,37 @@ class EditProfile extends React.Component{
 
     render(){
         return(
-            <View style={{padding: 16.0}}>
-                <ScrollView>
-                    <Heading style={{margin: 32.0, marginLeft: 16.0}}>Your Triggers</Heading>
-                </ScrollView>
+            <View style={{padding: 16.0, flex: 1}}>
+                <FlatList
+                    ListHeaderComponent={<Heading style={{margin: 32.0, marginLeft: 16.0}}>Your Triggers</Heading>}
+                    data={testData}
+                    keyExtractor={item => item.name}
+                    renderItem={({item}) => (<TriggerItem trigger={item} />)}
+                />
+
+                <FloatingAction
+                    actions={[
+                        {
+                            text: "Add Trigger",
+                            icon: <Icon name={'git-merge'} size={24} color={'#fff'} />,
+                            name: "bt_language",
+                            position: 1
+                        },
+                    ]}
+                    floatingIcon={<Icon name={'git-merge'} size={24} color={'#fff'} />}
+                    overrideWithAction
+                />
             </View>
         )
     }
+}
+
+const TriggerItem = props => {
+    return(
+        <View>
+
+        </View>
+    );
 }
 
 const mapStateToProps = state => ({
