938a938480723ff2c4f5fc86ee70f02eb7bc618e
sriPithani987 <53921979+sriPithani987@users.noreply.github.com>
2019-09-08 00:14:30 -0400

    Profile Build

--- src/react/screens/EditProfile/index.js
+++ src/react/screens/EditProfile/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, ScrollView } from 'react-native';
+import { View, ScrollView, StyleSheet, Image, TouchableOpacity } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
 
 import * as ActionTypes from '../../../redux/ActionTypes';
@@ -9,6 +9,31 @@ import { Heading, Text } from '../../components/StyledComponents';
 import { MaterialCommunityIcons as Icon } from '@expo/vector-icons'
 
 
+const testData = [
+    {
+        name: 'Test profile',
+        actions: [
+            {
+                id: 'testAction',
+                name: 'Jason',
+                testParam: 'testParam'
+            },
+            {
+                id: 'otherAcction',
+                name: 'Ayush',
+                type: 'air'
+
+            },
+
+            {
+                id: 'moreAction',
+                name: 'Guna',
+                type: 'music'
+
+            }
+        ]
+    }
+]
 class EditProfile extends React.Component{
     constructor(props){
         super(props)
@@ -20,10 +45,95 @@ class EditProfile extends React.Component{
                 <ScrollView>
                     <Heading style={{margin: 32.0, marginLeft: 16.0}}>Your Profile</Heading>
                 </ScrollView>
+                <View style = {styles.container}></View>
+                    <View style = {styles.header}></View>
+                        <Image style = {styles.avatar}></Image>
+                        <View style={styles.body}>
+            <View style={styles.bodyContent}>
+              <Text style={styles.name}>Ayush</Text>
+              <Text style={styles.info}>UX Designer / Mobile developer</Text>
+              <Text style={styles.description}>I'm a CS boy</Text>
+                   
+              <TouchableOpacity style={styles.buttonContainer}>
+                <Text>Friends</Text>  
+              </TouchableOpacity>              
+              <TouchableOpacity style={styles.buttonContainer}>
+                <Text>Favorites</Text> 
+              </TouchableOpacity>
             </View>
-        )
+        </View>
+      </View>
+
+                        
+                    
+               
+          
+        );
     }
 }
+
+const styles = StyleSheet.create({
+    header:{
+        backgroundColor: "#efefef",
+        height:200,
+    },
+    avatar:{
+          
+        width: 130,
+        height: 130,
+        borderRadius: 63,
+        borderWidth: 4,
+        borderColor: "white",
+        marginBottom:10,
+        alignSelf:'center',
+        position: 'absolute',
+        marginTop:300
+    },
+    name: {
+        fontSize: 22,
+        
+        fontWeight: '600',
+
+
+    },
+    body:{
+        marginTop:40,
+      },
+      bodyContent: {
+        flex: 1,
+        alignItems: 'center',
+        padding:30,
+      },
+      name:{
+        fontSize:28,
+       
+        fontWeight: "600"
+      },
+      info:{
+        fontSize:16,
+       
+        marginTop:10
+      },
+      description:{
+        fontSize:16,
+        
+        marginTop:20,
+        textAlign: 'center'
+      },
+      buttonContainer: {
+        marginTop:10,
+        height:45,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom:20,
+        width:250,
+        borderRadius:30,
+        backgroundColor: "#00BFFF",
+
+
+}
+});
 
 const mapStateToProps = state => ({
     // buildings: state.buildings.list,
